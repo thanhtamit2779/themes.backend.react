fe0876b747aa215d5952089e33375e9149cc13a2
ThanhTâm <thanhtam.it2779@gmail.com>
2018-04-07 17:30:25 +0700

    Clean code home page

--- src/app/modules/frontend/homepage/components/ThemeFeaturedCarousel.js
+++ src/app/modules/frontend/homepage/components/ThemeFeaturedCarousel.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 
-import * as _ from 'lodash';
-
-import { NavLink } from 'react-router-dom';
+import ProductList from './../../product/components/ProductList';
 
 class ThemeFeaturedCarousel extends Component {
     constructor(props) {
@@ -11,72 +9,37 @@ class ThemeFeaturedCarousel extends Component {
 
     componentDidUpdate() {
         jQuery('.owl-carousel-featured').owlCarousel({
-          loop: true,
-          responsiveClass: true,
-          responsive: {
-            0: {
-              items: 1,
-              nav: true
-            },
-            600: {
-              items: 3,
-              nav: false
-            },
-            768: {
-              items: 4,
-              nav: true,
-              loop: true,
-              margin: 30,
-              autoplay: true,
-              dots: false,
-              lazyLoad: true,
-              autoplayTimeout  : 3000
+            loop: true,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: true
+                },
+                600: {
+                    items: 3,
+                    nav: false
+                },
+                768: {
+                    items: 4,
+                    nav: true,
+                    loop: true,
+                    margin: 30,
+                    autoplay: true,
+                    dots: false,
+                    lazyLoad: true,
+                    autoplayTimeout: 3000
+                }
             }
-          }
         })
-      }
-    
-
-    load(posts) {
-        if (_.isEmpty(posts)) 
-            return false;
-        
-        return posts.map((post, key) => {
-            let link_detail = `chi-tiet/${post.post_slug}/${post.post_id}`;
-            return (
-                <div className="item" key={key}>
-                    <div className="block2">
-                        <div className="block2-img wrap-pic-w of-hidden pos-relative">
-                            <img src={post.post_thumb} alt={post.post_title}/>
-                            <div className="block2-overlay trans-0-4">
-                                <div className="block2-btn-addcart w-size1 trans-0-4">
-                                    {/* Button */}
-                                    <button className="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
-                                        ĐẶT MUA
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="block2-txt p-t-20 text-center">
-                            <NavLink to={link_detail} className="text-center block2-name dis-block s-text3 p-b-5">
-                                {post.post_title}
-                            </NavLink>
-                            <span className="block2-price m-text6 p-r-5">
-                                {post.post_price} VNĐ
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            )
-        });
     }
 
     render() {
         const posts = this.props.items.posts;
-        return (                     
+        return (
             <React.Fragment>
-                { this.load(posts) }
-            </React.Fragment>   
+                <ProductList posts={posts}/>
+            </React.Fragment>
         );
     }
 }
--- src/app/modules/frontend/homepage/components/ThemeViewedCarousel.js
+++ src/app/modules/frontend/homepage/components/ThemeViewedCarousel.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 
-import * as _ from 'lodash';
-
-import { NavLink } from 'react-router-dom';
+import ProductList from './../../product/components/ProductList';
 
 class ThemeViewedCarousel extends Component {
     constructor(props) {
@@ -35,46 +33,12 @@ class ThemeViewedCarousel extends Component {
         })
     }
 
-    load(posts) {
-        if (_.isEmpty(posts)) 
-            return false;
-            
-        return posts.map((post, key) => {
-            let link_detail = `chi-tiet/${post.post_slug}/${post.post_id}`;
-            return (
-                <div className="item" key={key}>
-                    <div className="block2">
-                        <div className="block2-img wrap-pic-w of-hidden pos-relative">
-                            <img src={post.post_thumb} alt={post.post_title}/>
-                            <div className="block2-overlay trans-0-4">
-                                <div className="block2-btn-addcart w-size1 trans-0-4">
-                                    {/* Button */}
-                                    <button className="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
-                                        ĐẶT MUA
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="block2-txt p-t-20 text-center">
-                            <NavLink to={link_detail} className="text-center block2-name dis-block s-text3 p-b-5">
-                                {post.post_title}
-                            </NavLink>
-                            <span className="block2-price m-text6 p-r-5">
-                                {post.post_price} VNĐ
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            )
-        });
-    }
-
     render() {
         const posts = this.props.items.posts;
-        return (
+        return (                     
             <React.Fragment>
-                {this.load(posts)}
-            </React.Fragment>
+                <ProductList posts={posts}/>
+            </React.Fragment>   
         );
     }
 }
